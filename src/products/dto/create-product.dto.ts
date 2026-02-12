07d2e7707d70a14e83fca265a1046b568270f440
ValeriiKorchahin <83233179+ValeriiKorchahin@users.noreply.github.com>
2026-02-12 23:31:17 +0200

    changed product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,26 @@
+import {
+  IsBoolean,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
 
+export class CreateProductDto {
+  @IsUUID()
+  companyId: string;
+
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  defaultPrice: number;
+
+  @IsUUID()
+  @IsOptional()
+  vatId?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+}
